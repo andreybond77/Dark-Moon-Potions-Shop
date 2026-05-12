50d733fc1207a592f7b2e8c10bdf11c2c50d852d
Андрей <155187683+andreybond77@users.noreply.github.com>
2026-05-12 22:12:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dark-Moon-Potions-Shop/',  // ← ТОЧНОЕ ИМЯ РЕПОЗИТОРИЯ
+  base: '/Dark-Moon-Potions-Shop/',
 })
